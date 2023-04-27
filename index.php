09c5d282dbdd50fe9c753ad545e2b8939da9f5c6
Lilyyyc <124380100+Lilyyyc@users.noreply.github.com>
2023-04-27 13:44:46 +0000

    Adding code in, finished index.php

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
 <DOCTYPE html>
+<html lang="en-ca">
+ <head>
+
+   <!--Meta data-->
+   <meta charset="utf-8">
+   <meta name="description" content="Waffles order">
+   <meta name="keywords" content="immaculata, ics2o">
+   <meta name="author" content="Lily">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+   <!--Favicon-->
+   <link rel="apple-touch-icon" sizes="180x180" href="./fav_index/apple-touch-icon.png">
+   <link rel="icon" type="image/png" sizes="32x32" href="./fav_index/favicon-32x32.png">
+   <link rel="icon" type="image/png" sizes="16x16" href="./fav_index/favicon-16x16.png">
+   <link rel="manifest" href="./fav_index/site.webmanifest">
+
+   <!--Link to CSS file-->
+   <link rel="stylesheet" href="./css/style.css">
+
+   <!--Title-->
+   <title>Waffles Order Webpage</title>
+ </head>
+ <body>
+
+   <!--MDL theme-->
+   <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.blue-indigo.min.css" /> 
+
+<!--Header-->
+<?php echo
