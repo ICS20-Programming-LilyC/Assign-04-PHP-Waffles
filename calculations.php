44dac92bc20fc2c5098d5fa3a1a8b36045a37782
Lilyyyc <124380100+Lilyyyc@users.noreply.github.com>
2023-04-28 20:37:38 +0000

    fixed indentation, comments, corrected spelling but favicon not working still

--- calculations.php
+++ calculations.php
@@ -6,13 +6,13 @@
   define("PRICE_FRIED_CHICKEN_TENDERS", 2.00);
   define("PRICE_BREAKFAST_DRINKS", 1.00);
   
-  // Initializing variable for base cost of waffles, which is determined depending on the size of waffle ordered.
+  // Initializing variables.
   $baseWaffleCost = 0;
   $numberOfWaffleToppings = 0;
   $numberOfFriedChickenTenders = 0;
   $numberOfBreakfastDrinks = 0;
 
-// Declaring my variables and matching them to my ids in index.php.
+  // Declaring my variables and matching them to my ids in index.php.
 
 if (isset($_POST["fried-chicken-tenders"])) {
   $numberOfFriedChickenTenders = intval($_POST["fried-chicken-tenders"]);
@@ -23,20 +23,21 @@ if (isset($_POST["amount-drinks"])) {
 }
 
 if ( isset($_POST["size"])){
-$waffleSize = $_POST["size"];
+  $waffleSize = $_POST["size"];
   }
 
- // Using determined value to declare variable for number of toppings chosen
+ // Using the determined value to declare a variable for number of toppings chosen.
 if(!empty($_POST["checkboxes"]) && is_array($_POST["checkboxes"])) {
    $numberOfWaffleToppings = count($_POST["checkboxes"]);
-} else {
+} 
+else {
    $numberOfWaffleToppings = 0;
 }
 
-  // Determining the cost of the toppings using number of toppings variable and cost of toppings constant
+  // Determining the cost of the toppings using the number of toppings variable and cost of toppings constant.
   $costOfToppings = $numberOfWaffleToppings * PRICE_WAFFLE_TOPPINGS;
 
-// If waffle size is not selected, user cannot order just toppings.
+  // If waffle size is not selected, the user cannot order just toppings.
 if ($waffleSize == "0") {
     $numberOfWaffleToppings = 0;
     $costOfToppings = 0;
@@ -68,11 +69,11 @@ if ($waffleSize == "0") {
   $costOfFriedChicken = $numberOfFriedChickenTenders * PRICE_FRIED_CHICKEN_TENDERS;
   $costOfBreakfastDrinks = $numberOfBreakfastDrinks * PRICE_BREAKFAST_DRINKS;
   
-  // Calculating the user's subtotal, tax and total for their order
+  // Calculating the user's subtotal, tax and total for their order.
   $subtotal = $baseWaffleCost + $costOfToppings + $costOfFriedChicken + $costOfBreakfastDrinks;
   $taxes = $subtotal * HST;
   $total = $subtotal + $taxes;
 
-  // Displaying the total to the user in the "results" iframe
+  // Displaying the subtotal, tax and total back to the user in the "results" iframe.
   echo "Your order is a " . $waffleSize . " waffle. For your waffle, you chose " . $numberOfWaffleToppings . " topping(s). You also decided to add " . $numberOfFriedChickenTenders . " fried chicken tender(s) to your order, along with " . $numberOfBreakfastDrinks . " beverage(s). Your subtotal is $" . number_format($subtotal,2) . ". The calculated amount of taxes added due to the HST rate is $" . number_format($taxes,2) . ". Which makes your total $" . number_format($total,2) . ". Thanks for your purchase!"; 
 ?>
--- index.php
+++ index.php
@@ -1,39 +1,39 @@
 <DOCTYPE html>
 <html lang="en-ca">
- <head>
+  <head>
 
-   <!--Meta data-->
-   <meta charset="utf-8">
-   <meta name="description" content="Waffles order">
-   <meta name="keywords" content="immaculata, ics2o">
-   <meta name="author" content="Lily">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Meta data-->
+    <meta charset="utf-8">
+    <meta name="description" content="Waffles order">
+    <meta name="keywords" content="immaculata, ics2o">
+    <meta name="author" content="Lily">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-   <!--Favicon-->
-   <link rel="apple-touch-icon" sizes="180x180" href="./favicon/apple-touch-icon.png">
-   <link rel="icon" type="image/png" sizes="32x32" href="./favicon/favicon-32x32.png">
-   <link rel="icon" type="image/png" sizes="16x16" href="./favicon/favicon-16x16.png">
-   <link rel="manifest" href="./favicon/site.webmanifest">
+    <!--Favicon-->
+    <link rel="apple-touch-icon" sizes="180x180" href="./favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./favicon/favicon-16x16.png">
+    <link rel="manifest" href="./favicon/site.webmanifest">
 
-   <!--Link to CSS file-->
-   <link rel="stylesheet" href="./css/style.css">
+    <!--Link to CSS file-->
+    <link rel="stylesheet" href="./css/style.css">
 
-   <!--Title-->
-   <title>Waffles Order Webpage</title>
- </head>
+    <!--Title-->
+    <title>Waffles Order Webpage</title>
+  </head>
   <body>
 
-   <!--MDL theme-->
-   <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.blue-indigo.min.css" /> 
+    <!--MDL theme-->
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.blue-indigo.min.css" />
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
 
-<!--Header-->
+    <!--Header-->
 <?php echo "<h1>Waffle Orders with PHP</h1>"; ?>
 
-<!--Text-->
-<?php echo "<p>Welcome to our waffles breakfast company. With this webpage you can place an order for a waffles with your choice of topping and an optional items. We have a variety of different sizes and toppings so that you can customize your own waffle. In addition with optional breakfast drinks and fried chicken tenders. We hope you enjoy your meal!</p>" ?>
+    <!--Text-->
+<?php echo "<p>Welcome to our waffles breakfast company. With this webpage you can place an order for a waffle with your choice of topping and optional items. We have a variety of different sizes and toppings so that you can customize your own waffle. In addition with optional breakfast drinks and fried chicken tenders. We hope you enjoy your meal!</p>" ?>
 
-<!--Image-->
+    <!--Image-->
     <center>
       <img src="./images/classicWaffles.jpeg" alt="Waffles" width="400" length="400">
     </center>
@@ -41,121 +41,122 @@
     <br>
 
     <!--Table for prices and user order (side-by-side)-->
-   <center>
-     <?php "<h4>Menu:</h4>" ?>
-   </center>
+    <center>
+<?php echo "<h4>Menu:</h4>" ?>
+    </center>
 
-    <!--Table of options including prices-->
-   <center>
-     <table>
-       <tr>
-         <th class="border">Sizes:</th>
-         <th class="border">Price:</th>
-       </tr>
-       <tr>
-         <td class="border">Small</td>
-         <td class="border">$3.00</td>
-       </tr>
-       <tr>
-         <td class="border">Medium</td>
-         <td class="border">$4.00</td>
-       </tr>
-       <tr>
-         <td class="border">Large</td>
-         <td class="border">$5.00</td>
-       </tr>
-       <tr>
-         <td class="border">Toppings</td>
-         <td class="border">$2.00 (each)</td>
-       </tr>
-       <tr>
-         <td class="border">Breakfast Drink (optional)</td>
-         <td class="border">$1.00</td>
-       </tr>
-       <tr>
-         <td class="border">Fried Chicken Tender (optional)</td>
-         <td class="border">$2.00</td>
-       </tr>
-     </table>
-     <br>
-   </center>
+    <!--Table of options prices and sizes-->
+    <center>
+      <table>
+        <tr>
+          <th class="border">Sizes:</th>
+          <th class="border">Price:</th>
+        </tr>
+        <tr>
+          <td class="border">Small</td>
+          <td class="border">$3.00</td>
+        </tr>
+        <tr>
+          <td class="border">Medium</td>
+          <td class="border">$4.00</td>
+        </tr>
+        <tr>
+          <td class="border">Large</td>
+          <td class="border">$5.00</td>
+        </tr>
+        <tr>
+          <td class="border">Toppings</td>
+          <td class="border">$2.00 (each)</td>
+        </tr>
+        <tr>
+          <td class="border">Breakfast Drink (optional)</td>
+          <td class="border">$1.00</td>
+        </tr>
+        <tr>
+          <td class="border">Fried Chicken Tender (optional)</td>
+          <td class="border">$2.00</td>
+        </tr>
+      </table>
+      <br>
+    </center>
 
- <!--Form to allow user input-->
-   <form action="./calculations.php" method="post" target="results">
-     <php echo <h4>Please indicate the size of waffle you would like:</h4>
+    <!--Form to allow user input-->
+    <form action="./calculations.php" method="post" target="results">
+      <?php echo "<h4>Please indicate the size of waffle you would like:</h4>"; ?>
+      <br>
 
-   <!--Multiple choice - picking size of pancake-->
-     <label for="size">Select a size of waffle for your order:</label>
-     <select id="size" name="size">
-       <option value="0">-- Size --</option>
-       <option value="small">Small</option>
-       <option value="medium">Medium</option>
-       <option value="large">Large</option>
-     </select>
-     <br>
-     <br>
-
-  <!--MDL checkboxes for pancake topping-->
-     <h4>Choose your waffle topping:</h4>
-     
-  <!--First checkbox - Original-->
-     <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect">
-       <span class="mdl-checkbox__label">Original</span>
-       <input type="checkbox" id="original" name="checkboxes[]" value="1" class="mdl-checkbox__input">
-     </label>
-     <br>
-     <br>
-
- <!--Second checkbox - Cinnamon & sugar-->
-     <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect">
-       <span class="mdl-checkbox__label">Cinnamon & sugar</span>
-       <input type="checkbox" id="cinnamon-and-sugar" name="checkboxes[]" value="1" class="mdl-checkbox__input">
-     </label>
-     <br>
-     <br>
-
-  <!--Third checkbox - Chocolate or nutella drizzle-->
-     <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect">
-       <span class="mdl-checkbox__label">Chocolate or nutella drizzle</span>
-       <input type="checkbox" id="chocolate-or-nutella-drizzle" name="checkboxes[]" value="1"  class="mdl-checkbox__input">
-     </label>
-     <br>
-     <br>
-
-  <!--Fourth checkbox - Whipped cream and fruit-->
-     <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect">
-       <span class="mdl-checkbox__label">Whipped cream and fruit</span>
-       <input type="checkbox" id="whipped-cream-and-fruit" name="checkboxes[]" value="1" class="mdl-checkbox__input">
-     </label>
+    <!--Multiple choice - picking size of pancake-->
+        <label for="size">Select a size of waffle for your order:</label>
+        <select id="size" name="size">
+          <option value="0">-- Size --</option>
+          <option value="small">Small</option>
+          <option value="medium">Medium</option>
+          <option value="large">Large</option>
+        </select>
       <br>
       <br>
 
-  <!--Fith checkbox - Fried Eggs and bacon-->
-     <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect">
-       <span class="mdl-checkbox__label">Fried Eggs and bacon</span>
-       <input type="checkbox" id="fried-eggs-and-bacon" name="checkboxes[]" value="1" class="mdl-checkbox__input">
-     </label>
+    <!--MDL checkboxes for pancake topping-->
+        <h4>Choose your waffle topping:</h4>
+
+    <!--First checkbox - Original-->
+        <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect">
+          <span class="mdl-checkbox__label">Original</span>
+          <input type="checkbox" id="original" name="checkboxes[]" value="1" class="mdl-checkbox__input">
+        </label>
       <br>
       <br>
 
-<!--Optional iteams-->
-     <h4>If you want to order any optional iteams, please select them below:</h4>
-     <h6>For our optional items we have, a variety of breakfast drinks, as well as our homemade fried chicken tenders.</h6>
+    <!--Second checkbox - Cinnamon & sugar-->
+        <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect">
+          <span class="mdl-checkbox__label">Cinnamon & sugar</span>
+          <input type="checkbox" id="cinnamon-and-sugar" name="checkboxes[]" value="1" class="mdl-checkbox__input">
+        </label>
+      <br>
+      <br>
 
-<!--Chicken tenders chexboxes-->
-     <p>Would you like chicken tenders with your meal? If not, select the "no" option and input "0" for the amount of chicken tenders required for your order.</p>
-     
-  <!--MDL radio button (Yes)-->
-     <label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="option-1">
-       <input type="radio" id="option-1" class="mdl-radio__button" name="options" value="1" checked>
-       <span class="mdl-radio__label">Yes</span>
-     </label>
+    <!--Third checkbox - Chocolate or nutella drizzle-->
+        <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect">
+          <span class="mdl-checkbox__label">Chocolate or nutella drizzle</span>
+          <input type="checkbox" id="chocolate-or-nutella-drizzle" name="checkboxes[]" value="1"  class="mdl-checkbox__input">
+        </label>
+      <br>
+      <br>
 
-  <!--MDL radio button (Off)-->
-     <label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="option-2">
-       <input type="radio" id="option-2" class="mdl-radio__button" name="options" value="2">
-       <span class="mdl-radio__label">No</span>
-     </label>
+    <!--Fourth checkbox - Whipped cream and fruit-->
+        <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect">
+          <span class="mdl-checkbox__label">Whipped cream and fruit</span>
+          <input type="checkbox" id="whipped-cream-and-fruit" name="checkboxes[]" value="1" class="mdl-checkbox__input">
+        </label>
+      <br>
+      <br>
+
+    <!--Fifth checkbox - Fried Eggs and bacon-->
+        <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect">
+          <span class="mdl-checkbox__label">Fried Eggs and bacon</span>
+          <input type="checkbox" id="fried-eggs-and-bacon" name="checkboxes[]" value="1" class="mdl-checkbox__input">
+        </label>
+      <br>
+      <br>
+
+    <!--Optional items radio buttons-->
+        <h4>If you want to order any optional items, please select them below:</h4>
+        <h6>For our optional items we have, a variety of breakfast drinks, as well as our homemade fried chicken tenders.</h6>
+
+    <!--Chicken tenders checkboxes-->
+        <p>Would you like chicken tenders with your meal? If not, select the "no" option and input "0" for the amount of chicken tenders required for your order.</p>
+
+    <!--MDL radio button (Yes)-->
+        <label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="option-1">
+          <input type="radio" id="option-1" class="mdl-radio__button" name="options" value="1" checked>
+          <span class="mdl-radio__label">Yes</span>
+        </label>
+
+    <!--MDL radio button (Off)-->
+        <label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="option-2">
+          <input type="radio" id="option-2" class="mdl-radio__button" name="options" value="2">
+          <span class="mdl-radio__label">No</span>
+        </label>
      <br>
      <br>
      <br>
@@ -169,34 +170,35 @@
        <option value="orange-juice">Orange Juice</option>
        <option value="apple-juice">Apple Juice</option>
      </select>
-     <br>
-     <br>
+      <br>
+      <br>
 
-<!--Amonut of user's optional iteams in their order-->
-     <h4>If you're ordering any of our optional items, then please indicate below the amount of drinks and/or  tenders you would like in your order:</h4>
+    <!--Amount of user's optional items in their order-->
+        <h4>If you're ordering any of our optional items, then please indicate below the amount of drinks and/or  tenders you would like in your order:</h4>
 
-  <!--Input for optional breakfast drink-->
+    <!--Input for optional breakfast drink-->
         <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-       <input class="mdl-textfield__input" type="number" id="amount-drinks" step="1" value="0" min="0"  name="amount-drinks">
-       <label class="mdl-textfield__label" for="amount-drinks">How many breakfast dinks would you like?</label>
-     </div>
+          <input class="mdl-textfield__input" type="number" id="amount-drinks" step="1" value="0" min="0"  name="amount-drinks">
+          <label class="mdl-textfield__label" for="amount-drinks">How many breakfast dinks would you like?</label>
+        </div>
 
-  <!--Input for optional fried chicken tenders-->
-       <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-       <input class="mdl-textfield__input" type="number" id="fried-chicken-tenders" step="1" value="0" min="0"  name="fried-chicken-tenders">
-       <label class="mdl-textfield__label" for="fried-chicken-tenders">How many fried chicken tenders would you like?</label>
-     </div>
-       <br>
-       <br>
+    <!--Input for optional fried chicken tenders-->
+        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+          <input class="mdl-textfield__input" type="number" id="fried-chicken-tenders" step="1" value="0" min="0"  name="fried-chicken-tenders">
+          <label class="mdl-textfield__label" for="fried-chicken-tenders">How many fried chicken tenders would you like?</label>
+        </div>
+      <br>
+      <br>
 
-  <!--Adding id to submit button to style button-->
-     <input type="submit" id="sumbit" value="Submit Your Order" button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-     </form>
-     <br>
-     <br>
+    <!--Adding id to submit button to style button-->
+        <input type="submit" id="submit" value="Submit Your Order" button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+      </form>
+      <br>
+      <br>
 
-  <!--iframe-->
-       <iframe name="results" id="results"></iframe>
-    <br>
-    <br>
-     </php>
+    <!--iframe-->
+        <iframe name="results" id="results"></iframe>
+      <br>
+      <br>
+  </body>
+</html>
